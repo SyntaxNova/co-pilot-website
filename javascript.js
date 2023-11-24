57a2918d90120dc26c6dadc64313b6ff67d12b39
“Atharva <atharvacodes@gmail.com>
2023-11-24 12:04:35 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+
+// Your JavaScript code goes here
+function greet() {
+    console.log("Hello, world!");
+}
+greet();
+
+
+// Your JavaScript code goes here
+<html>
+<head>
+    <title>My Website</title>
+    </head>
+<body>
+    <header>
+        <h1>Welcome to My Website</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>About Me</h2>
+            <p>This is some information about me.</p>
+        </section>
+
+        <section>
+            <h2>Contact</h2>
+            <form id="contactForm">
+                <input type="text" name="name" placeholder="Your Name" />
+                <input type="email" name="email" placeholder="Your Email" />
+                <textarea name="message" placeholder="Your Message"></textarea>
+                <button type="submit">Send Message</button>
+            </form>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2022 My Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
